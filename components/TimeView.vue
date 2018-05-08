65b608599bfd30fa7fa8df48e04c22660c3133e9
suzu2469 <subayai.kobashiri@gmail.com>
2018-05-09 06:26:53 +0900

    first commit

---
+++ components/TimeView.vue
@@ -0,0 +1,79 @@
+<template>
+  <div id="show-time">
+    <div class="time-inner">
+      <h1 class="time-row">
+        {{ currentTime.getMonth() }}/{{ currentTime.getDay() }}
+      </h1>
+      <h1 class="time-row">
+        {{ getHours }}&nbsp;<span class="time-coron">:</span>&nbsp;{{ getMinutes }}
+      </h1>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      currentTime: null
+    }
+  },
+  created () {
+    this.setCurrentTime()
+    this.handleTime()
+  },
+  computed: {
+    getHours () {
+      return ('00' + this.currentTime.getHours()).slice(-2)
+    },
+    getMinutes () {
+      return ('00' + this.currentTime.getMinutes()).slice(-2)
+    },
+    getSeconds () {
+      return ('00' + this.currentTime.getSeconds()).slice(-2)
+    }
+  },
+  methods: {
+    async handleTime () {
+      const sleep = ms => new Promise(resolve => setTimeout(resolve, ms))
+      while (true) {
+        await sleep(1000)
+        this.setCurrentTime()
+      }
+    },
+    setCurrentTime () {
+      this.currentTime = new Date
+    }
+  }
+}
+</script>
+
+<style scoped>
+#show-time {
+  height: 100vh;
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.time-row {
+  font-size: 120px;
+  text-align: center;
+}
+.time-coron {
+  animation: flashing-in ease 2s infinite;
+}
+
+@keyframes flashing-in {
+  0% {
+    color: inherit;
+  }
+  50% {
+    color: transparent;
+  }
+  100% {
+    color: inherit;
+  }
+}
+</style>
+
---
+++ pages/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <section id="time-page">
+    <time-view />
+  </section>
+</template>
+
+<script>
+import TimeView from '~/components/TimeView.vue'
+
+export default {
+  components: {
+    TimeView
+  }
+}
+</script>
+
+<style>
+
+</style>
